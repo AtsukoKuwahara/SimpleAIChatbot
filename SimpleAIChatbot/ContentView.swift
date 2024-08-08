cd00f8a2167ecd04d0a320e14d43a294e47c2b26
Atsuko Kuwahara <kuwahara.atsuko@aifocal.ai>
2024-08-08 16:20:38 -0400

    Initial Commit

---
+++ SimpleAIChatbot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleAIChatbot
+//
+//  Created by Atsuko Kuwahara on 2024-08-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleAIChatbot/SimpleAIChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAIChatbotApp.swift
+//  SimpleAIChatbot
+//
+//  Created by Atsuko Kuwahara on 2024-08-08.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAIChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
